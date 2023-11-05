0826f40a1582ed88d803be1bfb3122cf35634ac8
fsam <sam.flaujat@ynov.com>
2023-11-05 15:46:18 +0100

    1

--- src/Game/main.go
+++ src/Game/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"html/template"
+	"goquery"
 )
 
-const hangmanImage = document.querySelector(".hangman-box img")
-const wordDisplay = document.querySelector(".word-display")
-const guessesText = document.querySelector(".guesses-text b")
-const keyboardDiv = document.querySelector(".keyboard")
-const gameModal = document.querySelector(".game-modal")
+const hangmanImage = goquery(".hangman-box img")
+//const wordDisplay = document.querySelector(".word-display")
+//const guessesText = document.querySelector(".guesses-text b")
+//const keyboardDiv = document.querySelector(".keyboard")
+//const gameModal = document.querySelector(".game-modal")
 
-type Game struct {
-	currentWord []string
-	correctletters []rune
-	wrongGuessCount int
-	maxGuesses = 10
-}
+//type Game struct {
+//	currentWord []string
+//	correctletters []rune
+//	wrongGuessCount int
+//	maxGuesses = 10
+//}
 
 var tmpl *template.Template
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTeamplate(w, "index.html", nil)
+	tmpl.ExecuteTemplate(w, "index.html", nil)
 }
 
 func main() {
